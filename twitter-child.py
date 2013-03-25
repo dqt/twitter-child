cb8417bd4efe67a94ce0ca025efb343ed807e0cb
Thomas Graves <pythonbabe@gmail.com>
2013-03-25 04:05:52 +0000

    changed to 0Auth login. basic auth no longer supported

--- twitter-child.py
+++ twitter-child.py
@@ -33,34 +33,45 @@ f2 = logging.Formatter("%(levelname)s: %(message)s")
 h2.setFormatter(f2)
 x.addHandler(h2)
 
-username = 'decrypts'
-password = ''
 
-x.info('Using twitter username %s: ', username)
-password = raw_input('Please enter password: ')
-x.debug('Logging into twitter using %s:%s', username, password)
+# == OAuth Authentication ==
+#
+# This mode of authentication is the new preferred way
+# of authenticating with Twitter.
+
+# The consumer keys can be found on your application's Details
+# page located at https://dev.twitter.com/apps (under "OAuth settings")
+consumer_key="bKKGk5LL7zwfaQrO8Oew"
+consumer_secret=""
+
+# The access tokens can be found on your applications's Details
+# page located at https://dev.twitter.com/apps (located
+# under "Your access token")
+access_token="928433282-5PK07buv76ah499iW2NfabrO4l5K1cpDOLtRRm6s"
+access_token_secret=""
+
+
+x.debug('Getting consumer secret from user')
+consumer_secret = raw_input('Please enter consumer secret: ')
+x.debug('Getting access token secret from user')
+access_token_secret = raw_input('Please enter access token secret: ')
 
 try:
-    auth = tweepy.auth.BasicAuthHandler(username, password)
-    x.info('Login successful using %s:%s', username, password)
+    x.info('Attempting 0Auth login')
+    x.debug('Authenticating...')
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    x.debug('Setting access token...')
+    auth.set_access_token(access_token, access_token_secret)
+    x.info('Logged into %s', api.me().name)
 except Exception, err:
     x.warning('Login failed!')
-    x.debug('Login failed using %s:%s', username, password)
     x.warning('Program will exit!')
     x.exception(err)
     sys.exit(1)
 
 api = tweepy.API(auth)
-
-api.update_status('Updating using basic authentication via Tweepy!')
-
-
-
-
-
-
-
-
+x.info('Attempting status update')
+api.update_status('Updating using OAuth authentication via Tweepy!')
 
 
 
